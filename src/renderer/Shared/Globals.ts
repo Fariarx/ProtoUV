64577c8e09b8bc8c0898fffc558c95b44879c9bd
Владислав Чалый <chall4@yandex.ru>
2022-10-26 12:13:54 +0300

    _

--- src/renderer/Shared/Globals.ts
+++ src/renderer/Shared/Globals.ts
@@ -1,8 +1,6 @@
 import { BufferGeometry, Mesh } from 'three';
 import { acceleratedRaycast, computeBoundsTree, disposeBoundsTree } from 'three-mesh-bvh';
 
-setInterval(() => console.log(window.electron), 2000);
-
 export const bridge = window.electron.ipcRenderer.window;
 
 export const AppName = 'protouv';
@@ -10,3 +8,4 @@ export const AppName = 'protouv';
 BufferGeometry.prototype.computeBoundsTree = computeBoundsTree;
 BufferGeometry.prototype.disposeBoundsTree = disposeBoundsTree;
 Mesh.prototype.raycast = acceleratedRaycast;
+
